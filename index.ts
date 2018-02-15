3963415ec655971f0879446c74af54531451a282
Tim Perry <pimterry@gmail.com>
2018-02-15 22:17:59 +0100

    Allow nested lookup through possibly undefined propeties

--- index.ts
+++ index.ts
@@ -1,23 +1,36 @@
-function get<T, S1 extends keyof T, S2 extends keyof (T[S1])>(
+function get<
+    T,
+    S1 extends keyof T,
+    R2 extends T[S1] & {},
+    S2 extends keyof R2
+>(
     obj: T,
     prop1: S1,
     prop2: S2
-): T[S1][S2];
-function get<T, S1 extends keyof T>(
+): R2 | undefined;
+function get<
+    T,
+    S1 extends keyof T
+>(
     obj: T,
     prop1: S1
 ): T[S1];
-function get<T, S1 extends keyof T, S2 extends keyof (T[S1])>(
+function get<
+    T,
+    S1 extends keyof T,
+    S2 extends keyof T[S1]
+>(
     obj: T,
     prop1: S1,
     prop2?: S2
-):  T[S1] | T[S1][S2] {
+):  T[S1] | T[S1][S2] | undefined {
     const props = [].slice.call(arguments, 1);
     let value: any = obj;
 
     while (props.length > 0) {
         let nextProp = props.shift();
         value = value[nextProp];
+        if (value == null) return undefined;
     }
 
     return value;
--- test.ts
+++ test.ts
@@ -21,4 +21,28 @@ describe("typesafe-get", () => {
         const input = { a: { b: 1 } };
         expect(get(input, 'a', 'b')).to.equal(1);
     });
+
+    it("lets you look up nested properties that may be undefined", () => {
+        const input: { a?: { b: number } } = { };
+        let result = get(input, 'a', 'b');
+        expect(result).to.equal(undefined);
+    });
+
+    it("correctly infers the type of a nested possibly-null property", () => {
+        const input: { a?: { b: { c: number } } } = { a: { b: { c: 5 } } };
+        let result = get(input, 'a', 'b');
+        expect(result!.c).to.equal(5);
+    });
+
+    it("returns null if the final property is null", () => {
+        const input: { a: number | null } = { a: null };
+        let result = get(input, 'a');
+        expect(result).to.equal(null);
+    });
+
+    it("returns undefined if following a path through a null", () => {
+        const input: { a: { b: number } | null } = { a: null };
+        let result = get(input, 'a', 'b');
+        expect(result).to.equal(undefined);
+    });
 });
