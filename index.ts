4dc508f3f345206d28d69b9db9752b39ea400240
Tim Perry <pimterry@gmail.com>
2018-07-17 18:00:44 +0200

    Add little simplification & tidy up

--- index.ts
+++ index.ts
@@ -1,9 +1,8 @@
 // Given an object T - returns a type with undefined/null removed from
 // the types of any fields.
-export type Defined<T> = {
-    [K in keyof T]-?: Exclude<T[K], null>
-};
-export type Prop<T, S extends keyof T> = Defined<T>[S];
+export type Defined<T> = Exclude<Exclude<T, null>, undefined>;
+// The value of T[S], if it's not null/undefined
+export type Prop<T, S extends keyof T> = Defined<T[S]>;
 
 export function get<
     T,
@@ -13,13 +12,14 @@ export function get<
     S4 extends keyof Prop<Prop<Prop<Defined<T>, S1>, S2>, S3>,
     S5 extends keyof Prop<Prop<Prop<Prop<Defined<T>, S1>, S2>, S3>, S4>
 >(
-    obj: T | undefined,
+    obj: T,
     prop1: S1,
     prop2: S2,
     prop3: S3,
     prop4: S4,
     prop5: S5,
 ): Prop<Prop<Prop<Prop<Defined<T>, S1>, S2>, S3>, S4>[S5] | undefined;
+
 export function get<
     T,
     S1 extends keyof Defined<T>,
@@ -27,39 +27,43 @@ export function get<
     S3 extends keyof Prop<Prop<Defined<T>, S1>, S2>,
     S4 extends keyof Prop<Prop<Prop<Defined<T>, S1>, S2>, S3>
 >(
-    obj: T | undefined,
+    obj: T,
     prop1: S1,
     prop2: S2,
     prop3: S3,
     prop4: S4,
 ): Prop<Prop<Prop<Defined<T>, S1>, S2>, S3>[S4] | undefined;
+
 export function get<
     T,
     S1 extends keyof Defined<T>,
     S2 extends keyof Prop<Defined<T>, S1>,
     S3 extends keyof Prop<Prop<Defined<T>, S1>, S2>
 >(
-    obj: T | undefined,
+    obj: T,
     prop1: S1,
     prop2: S2,
     prop3: S3,
 ): Prop<Prop<Defined<T>, S1>, S2>[S3] | undefined;
+
 export function get<
     T,
     S1 extends keyof Defined<T>,
     S2 extends keyof Prop<Defined<T>, S1>
 >(
-    obj: T | undefined,
+    obj: T,
     prop1: S1,
     prop2: S2
 ): Prop<Defined<T>, S1>[S2] | undefined;
+
 export function get<
     T,
     S1 extends keyof Defined<T>
 >(
-    obj: T | undefined,
+    obj: T,
     prop1: S1
-): Prop<Defined<T>, S1> | undefined;
+): Defined<T>[S1] | undefined;
+
 export function get<T>(
     obj: T,
     ...props: string[]
