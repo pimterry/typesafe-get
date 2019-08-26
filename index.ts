067f4f85a09e9561ccae5ada1b7fc3140565326d
Tim Perry <pimterry@gmail.com>
2019-08-26 14:42:22 +0200

    Replace Defined<X> with the builtin NonNullable<X>

--- index.ts
+++ index.ts
@@ -1,16 +1,13 @@
-// Given an object T - returns a type with undefined/null removed from
-// the types of any fields.
-export type Defined<T> = Exclude<T, null | undefined>;
 // The value of T[S], if it's not null/undefined
-export type Prop<T, S extends keyof T> = Defined<T[S]>;
+export type Prop<T, S extends keyof T> = NonNullable<T[S]>;
 
 export function get<
     T,
-    S1 extends keyof Defined<T>,
-    S2 extends keyof Prop<Defined<T>, S1>,
-    S3 extends keyof Prop<Prop<Defined<T>, S1>, S2>,
-    S4 extends keyof Prop<Prop<Prop<Defined<T>, S1>, S2>, S3>,
-    S5 extends keyof Prop<Prop<Prop<Prop<Defined<T>, S1>, S2>, S3>, S4>
+    S1 extends keyof NonNullable<T>,
+    S2 extends keyof Prop<NonNullable<T>, S1>,
+    S3 extends keyof Prop<Prop<NonNullable<T>, S1>, S2>,
+    S4 extends keyof Prop<Prop<Prop<NonNullable<T>, S1>, S2>, S3>,
+    S5 extends keyof Prop<Prop<Prop<Prop<NonNullable<T>, S1>, S2>, S3>, S4>
 >(
     obj: T,
     prop1: S1,
@@ -18,51 +15,51 @@ export function get<
     prop3: S3,
     prop4: S4,
     prop5: S5,
-): Prop<Prop<Prop<Prop<Defined<T>, S1>, S2>, S3>, S4>[S5] | undefined;
+): Prop<Prop<Prop<Prop<NonNullable<T>, S1>, S2>, S3>, S4>[S5] | undefined;
 
 export function get<
     T,
-    S1 extends keyof Defined<T>,
-    S2 extends keyof Prop<Defined<T>, S1>,
-    S3 extends keyof Prop<Prop<Defined<T>, S1>, S2>,
-    S4 extends keyof Prop<Prop<Prop<Defined<T>, S1>, S2>, S3>
+    S1 extends keyof NonNullable<T>,
+    S2 extends keyof Prop<NonNullable<T>, S1>,
+    S3 extends keyof Prop<Prop<NonNullable<T>, S1>, S2>,
+    S4 extends keyof Prop<Prop<Prop<NonNullable<T>, S1>, S2>, S3>
 >(
     obj: T,
     prop1: S1,
     prop2: S2,
     prop3: S3,
     prop4: S4,
-): Prop<Prop<Prop<Defined<T>, S1>, S2>, S3>[S4] | undefined;
+): Prop<Prop<Prop<NonNullable<T>, S1>, S2>, S3>[S4] | undefined;
 
 export function get<
     T,
-    S1 extends keyof Defined<T>,
-    S2 extends keyof Prop<Defined<T>, S1>,
-    S3 extends keyof Prop<Prop<Defined<T>, S1>, S2>
+    S1 extends keyof NonNullable<T>,
+    S2 extends keyof Prop<NonNullable<T>, S1>,
+    S3 extends keyof Prop<Prop<NonNullable<T>, S1>, S2>
 >(
     obj: T,
     prop1: S1,
     prop2: S2,
     prop3: S3,
-): Prop<Prop<Defined<T>, S1>, S2>[S3] | undefined;
+): Prop<Prop<NonNullable<T>, S1>, S2>[S3] | undefined;
 
 export function get<
     T,
-    S1 extends keyof Defined<T>,
-    S2 extends keyof Prop<Defined<T>, S1>
+    S1 extends keyof NonNullable<T>,
+    S2 extends keyof Prop<NonNullable<T>, S1>
 >(
     obj: T,
     prop1: S1,
     prop2: S2
-): Prop<Defined<T>, S1>[S2] | undefined;
+): Prop<NonNullable<T>, S1>[S2] | undefined;
 
 export function get<
     T,
-    S1 extends keyof Defined<T>
+    S1 extends keyof NonNullable<T>
 >(
     obj: T,
     prop1: S1
-): Defined<T>[S1] | undefined;
+): NonNullable<T>[S1] | undefined;
 
 export function get<T>(
     obj: T,
